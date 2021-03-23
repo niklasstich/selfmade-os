8fa0a06c1542099b772203454906c2aebae3ef0b
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-03-23 21:22:00 +0100

    string functions and runtime for arrays

--- sjc/rte.java
+++ sjc/rte.java
@@ -9,6 +9,10 @@ package java.lang;
 public class String {
 	private char[] value;
 	private int count;
+	public String(char[] value) {
+		this.value = value;
+		this.count = value.length;
+	}
 	@SJC.Inline
 	public int length() {
 		return count;
@@ -16,6 +20,33 @@ public class String {
 	@SJC.Inline
 	public char charAt(int i) {
 		return value[i];
+	}
+	@SJC.Inline
+	public char[] toCharArray() {
+		return value;
+	}
+	@SJC.Inline
+	public String reverse() {
+		char[] rev = new char[count];
+		for (int i=0,j=count-1;i<=j;i++,j--) {
+			rev[i] = value[j];
+			rev[j] = value[i];
+		}
+		return new String(rev);
+	}
+	@SJC.Inline
+	public String concat(String s) {
+		char[] buf = new char[s.count+this.count];
+		//copy over this string first
+		int index = 0;
+		for(int i = 0; i<this.count; i++) {
+			buf[index++] = this.value[i];
+		}
+		//copy over s after
+		for (int i = 0; i<s.count; i++) {
+			buf[index++] = s.value[i];
+		}
+		return new String(buf);
 	}
 }
 package rte;
@@ -51,12 +82,12 @@ public class DynamicRuntime {
 		if (ob == null) {
 			while(true);
 		}
-		objPtr += ob._r_scalarSize + ob._r_relocEntries*4;
 		while(ob._r_next != null) {
-			//traverse objects while updating pointer
+			//traverse objects
 			ob = ob._r_next;
-			objPtr += ob._r_scalarSize + ob._r_relocEntries*4;
 		}
+		//return to pointer and add scalar size after last object
+		objPtr = MAGIC.cast2Ref(ob)+ob._r_scalarSize;
 		//align to 4
 		if(objPtr % 4 != 0){
 			objPtr += 4 - (objPtr % 4);
@@ -72,26 +103,56 @@ public class DynamicRuntime {
 		MAGIC.assign(newOb._r_type, type);
 		return newOb;
 	}
+	//creates a new array object
 	public static SArray newArray(int length, int arrDim, int entrySize,
 	                              int stdType, Object unitType) {
 		int scalarSize = MAGIC.getInstScalarSize("SArray");
 		int relocCount = MAGIC.getInstScalarSize("SArray");
 		//if the array is multidimensional or holds relocs, we add the length of it to the reloc count
 		if (arrDim>1 || entrySize<0) relocCount+=length;
-		else scalarSize+=length*entrySize //otherwise, its elements are scalars, so we add to that count
+		else scalarSize+=length*entrySize; //otherwise, its elements are scalars, so we add to that count
 		SArray arr = (SArray)newInstance(scalarSize, relocCount, MAGIC.clssDesc("SArray"));
 		//overwrite proper attributes
 		MAGIC.assign(arr.length, length);
 		MAGIC.assign(arr._r_unitType, unitType);
 		MAGIC.assign(arr._r_stdType, stdType);
-		MAGIC.assign(arr._r_dim);
+		MAGIC.assign(arr._r_dim, arrDim);
 		return arr;
 	}
+	//create a new multi level array
+	//aus dem Handbuch kopiert :/
 	public static void newMultArray(SArray[] parent, int curLevel,
 	                                int destLevel, int length, int arrDim, int entrySize, int stdType,
-	                                Object unitType) { while(true); }
+	                                Object unitType) {
+		int i; //temporäre Variable
+		if (curLevel+1<destLevel) { //es folgt noch mehr als eine Dimension
+			curLevel++; //aktuelle Dimension erhöhen
+			for (i=0; i<parent.length; i++) //jedes Element mit Array befüllen
+			newMultArray((SArray[])((Object)parent[i]), curLevel, destLevel,
+			length, arrDim, entrySize, stdType, unitType);
+		}
+		else { //letzte anzulegende Dimension
+			destLevel=arrDim-curLevel; //Zieldimension eines Elementes
+			for (i=0; i<parent.length; i++) //jedes Element mit Zieltyp befüllen
+			parent[i]=newArray(length, destLevel, entrySize, stdType, unitType);
+		}
+	}
+	//aus dem Handbuch kopiert :/
 	public static boolean isInstance(Object o, SClassDesc dest,
-	                                 boolean asCast) { while(true); }
+	                                 boolean asCast) {
+		SClassDesc check; //temporäre Variable
+		if (o==null) { //Prüfung auf null
+			if (asCast) return true; //null darf immer konvertiert werden
+			return false; //null ist keine Instanz
+		}
+		check=o._r_type; //für weitere Vergleiche Objekttyp ermitteln
+		while (check!=null) { //suche passende Klasse
+			if (check==dest) return true; //passende Klasse gefunden
+			check=check.parent; //Elternklasse versuchen
+		}
+		if (asCast) while(true); //Konvertierungsfehler
+		return false; //Objekt passt nicht zu Klasse
+	}
 	public static SIntfMap isImplementation(Object o, SIntfDesc dest,
 	                                        boolean asCast) { while(true); }
 	public static boolean isArray(SArray o, int stdType,
